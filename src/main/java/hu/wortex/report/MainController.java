a403aa29a067916436f24c0648fdb28ae5a31e7b
SovietSpy2 <sovietspy2@yandex.com>
2019-01-30 18:59:02 +0100

    controller not needed atm

--- src/main/java/hu/wortex/report/MainController.java
+++ src/main/java/hu/wortex/report/MainController.java
@@ -1,61 +1,78 @@
-package hu.wortex.report;
-
-import hu.wortex.report.entities.Listing;
-import hu.wortex.report.repositories.BookRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-
-@Controller
-@RequestMapping(path="/")
-public class MainController {
-    @Autowired // This means to get the bean called userRepository
-    private BookRepository bookRepository;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    private static final Logger log = LoggerFactory.getLogger(MainController.class);
-
-    @GetMapping(path="/syncListings") // Map ONLY GET Requests
-    public @ResponseBody String syncListings () {
-
-        ResponseEntity<List<Listing>> rateResponse =
-                restTemplate.exchange("http://5c50abb1ee97f60014048115.mockapi.io/boki/BOOK",
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Listing>>() {
-                        });
-        List<Listing> rates = rateResponse.getBody();
-
-        // VALIDACIO ES MENTES
-
-        return "processed";
-    }
-
-    @GetMapping(path="/syncLocation")
-    public @ResponseBody String syncLocation() {
-
-        return "Ok";
-    }
-
-    @GetMapping(path="/syncListingStatus")
-    public @ResponseBody String syncListingStatus() {
-
-        return "Ok";
-    }
-
-    @GetMapping(path="/syncMarketplace")
-    public @ResponseBody String syncMarketplace() {
-
-        return "Ok";
-    }
-}
+//package hu.wortex.report;
+//
+//import hu.wortex.report.entities.Listing;
+//import hu.wortex.report.entities.Marketplace;
+//import hu.wortex.report.repositories.ListingRepository;
+//import hu.wortex.report.repositories.ListingStatusRepository;
+//import hu.wortex.report.repositories.LocationRepository;
+//import hu.wortex.report.repositories.MarketplaceRepository;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.ParameterizedTypeReference;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.List;
+//
+//@Controller
+//@RequestMapping(path="/")
+//public class MainController {
+//    @Autowired
+//    private ListingRepository listingRepository;
+//
+//    @Autowired
+//    private MarketplaceRepository marketplaceRepository;
+//
+//    @Autowired
+//    private ListingStatusRepository listingStatusRepository;
+//
+//    @Autowired
+//    private LocationRepository locationRepository;
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+//
+//    @GetMapping(path="/syncListings") // Map ONLY GET Requests
+//    public @ResponseBody String syncListings () {
+//
+//        ResponseEntity<List<Listing>> rateResponse =
+//                restTemplate.exchange("https://my.api.mockaroo.com/listing?key=63304c70",
+//                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Listing>>() {
+//                        });
+//        List<Listing> rates = rateResponse.getBody();
+//
+//        // VALIDACIO ES MENTES
+//
+//        return "processed";
+//    }
+//
+//    @GetMapping(path="/process")
+//    public @ResponseBody String syncLocation() {
+//
+//        Listing listing = listingRepository.findById("1").get();
+//        log.info(String.valueOf(listing.getListingStatus()));
+//
+//
+//        return "Ok";
+//    }
+//
+//    @GetMapping(path="/syncListingStatus")
+//    public @ResponseBody String syncListingStatus() {
+//
+//        return "Ok";
+//    }
+//
+//    @GetMapping(path="/syncMarketplace")
+//    public @ResponseBody String syncMarketplace() {
+//
+//        return "Ok";
+//    }
+//}
