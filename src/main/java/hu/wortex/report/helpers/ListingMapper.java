91f5049f8ed9f4d7f65be8b4f02ca27ba97fb74c
SovietSpy2 <sovietspy2@yandex.com>
2019-01-31 16:50:06 +0100

    refactored class and added csv generation

--- src/main/java/hu/wortex/report/helpers/ListingMapper.java
+++ src/main/java/hu/wortex/report/helpers/ListingMapper.java
@@ -1,75 +1,127 @@
 package hu.wortex.report.helpers;
 
 import hu.wortex.report.entities.*;
+import hu.wortex.report.repositories.ListingRepository;
 import hu.wortex.report.repositories.ListingStatusRepository;
 import hu.wortex.report.repositories.LocationRepository;
 import hu.wortex.report.repositories.MarketplaceRepository;
-import hu.wortex.report.services.ListingLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 
 @Service
 public class ListingMapper {
+
+    private static final Logger log = LoggerFactory.getLogger(ListingMapper.class);
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private ListingRepository listingRepository;
 
     @Autowired
     private ListingStatusRepository listingStatusRepository;
 
     @Autowired
+    private LocationRepository locationRepository;
+
+    @Autowired
     private MarketplaceRepository marketplaceRepository;
 
     @Autowired
-    private LocationRepository locationRepository;
+    private Environment env;
 
-    private static final Logger log = LoggerFactory.getLogger(ListingMapper.class);
+    /**
+     * saves valid Listings to the database
+     * @param dtos
+     */
+    @Transactional
+    public void handleSave(List<ListingDTO> dtos) throws IOException {
+
+        /// get lists here
+        List<String> locationIds = getLocationIds();
+        log.info("location id list for validation received");
+        List<Integer> marketplaceIds = getMarketplaceIds();
+        log.info("marketplace id list for validation received");
+        List<Integer> listingStatusIds = getListingStatusIds();
+        log.info("listing status id list for validation received");
+
+        Map<Integer,String> marketplacesMap = createIdNameMarketplaceMap();
 
 
-    public Listing mapToEntity(ListingDTO dto) {
+        ValidationUtility validationUtility = new ValidationUtility(locationIds, marketplaceIds, listingStatusIds,marketplacesMap);
 
+        listingRepository.saveAll(dtos.stream()
+                .filter(validationUtility::isValidListing)
+                .map(dto-> {
+                    Marketplace marketplace = entityManager.getReference(Marketplace.class, dto.getMarketPlaceId());
+                    Location location = entityManager.getReference(Location.class, dto.getLocationId());
+                    ListingStatus listingStatus = entityManager.getReference(ListingStatus.class, dto.getListingStatusId());
 
-        log.info("started loading lists");
+                    Listing listing = new Listing();
+                    listing.setId(dto.getId());
+                    listing.setCurrency(dto.getCurrency());
+                    listing.setDescription(dto.getDescription());
 
-        List<Marketplace> mks = (List<Marketplace>) marketplaceRepository.findAll();
-        List<Location> lks = (List<Location>) locationRepository.findAll();
-        List<ListingStatus> lts = (List<ListingStatus>) listingStatusRepository.findAll();
+                    listing.setListingPrice(dto.getListingPrice());
+                    listing.setOwnerEmailAddress(dto.getOwnerEmailAddress());
+                    listing.setQuantity(dto.getQuantity());
+                    listing.setTitle(dto.getTitle());
+                    listing.setUploadTime(dto.getUploadTime());
 
-        log.info("list loading done");
+                    listing.setListingStatus(listingStatus);
 
+                    listing.setMarketPlace(marketplace);
 
+                    listing.setLocation(location);
+                    log.info(listing.getId());
+                    entityManager.persist(listing);
+                    return listing;
+                }
+        ).collect(Collectors.toList()));
+        log.info("saving service done");
 
-        Listing listing = new Listing();
+        log.info("generating CSV file");
+        CsvGenerator csvGenerator = new CsvGenerator(env.getProperty("csv.file.location"));
+        csvGenerator.generateCsv(validationUtility.getCsvLines());
+        log.info("CSV file generating is done");
 
-        listing.setId(dto.getId());
-        listing.setCurrency(dto.getCurrency());
-        listing.setDescription(dto.getDescription());
-        listing.setListingPrice(dto.getListingPrice());
-        listing.setOwnerEmailAddress(dto.getOwnerEmailAddress());
-        listing.setQuantity(dto.getQuantity());
-        listing.setTitle(dto.getTitle());
-        listing.setUploadTime(dto.getUploadTime());
-
-        //listing.setListingStatus(lks.stream().filter(a->a.getId().equals(dto.getListingStatusId())).findFirst());
-
-        listing.setMarketPlace(findMarketplace(dto.getMarketPlaceId()));
-
-        listing.setLocation(findLocation(dto.getLocationId()));
-
-        return listing;
     }
 
-    private ListingStatus findListingStatus(Integer id) {
-        return listingStatusRepository.findById(id).orElse(null);
+    private List<Integer> getListingStatusIds() {
+        return StreamSupport.stream(listingStatusRepository.findAll().spliterator(), true)
+                .map(ListingStatus::getId)
+                .collect(Collectors.toList());
     }
 
-    private Marketplace findMarketplace(Integer id) {
-        return marketplaceRepository.findById(id).orElse(null);
+    private List<Integer> getMarketplaceIds() {
+        return StreamSupport.stream(marketplaceRepository.findAll().spliterator(), true)
+                .map(Marketplace::getId)
+                .collect(Collectors.toList());
     }
 
-    private Location findLocation(String id) {
-        return locationRepository.findById(id).orElse(null);
+    private List<String> getLocationIds() {
+        return StreamSupport.stream(locationRepository.findAll().spliterator(), true)
+                .map(Location::getId)
+                .collect(Collectors.toList());
     }
+
+    private Map<Integer,String> createIdNameMarketplaceMap() {
+        return StreamSupport.stream(marketplaceRepository.findAll().spliterator(),true)
+               .collect(Collectors.toMap(Marketplace::getId,Marketplace::getMarketplaceName ));
+    }
+
 
 }
