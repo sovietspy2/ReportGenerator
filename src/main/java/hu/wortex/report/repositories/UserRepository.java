0a68bab45b1bd273526596f28478e7aea3f1ac11
SovietSpy2 <sovietspy2@yandex.com>
2019-01-31 23:59:45 +0100

    refactor

--- src/main/java/hu/wortex/report/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.wortex.report.repositories;
-
-import hu.wortex.report.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
