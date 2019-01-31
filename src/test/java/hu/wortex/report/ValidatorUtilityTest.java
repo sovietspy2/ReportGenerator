41a54263bf8a910c849442c4ccffc3b67798002c
SovietSpy2 <sovietspy2@yandex.com>
2019-01-31 16:49:15 +0100

    todo implement tests

---
+++ src/test/java/hu/wortex/report/ValidatorUtilityTest.java
@@ -0,0 +1,25 @@
+package hu.wortex.report;
+
+import hu.wortex.report.helpers.Field;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+public class ValidatorUtilityTest {
+
+    @Test
+    public void testValidator() {
+
+        //TODO: add more test for every possible case
+
+    }
+
+
+
+}
