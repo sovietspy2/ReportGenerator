25324a75f9345d8b015fe36cea0306a6f7102137
SovietSpy2 <sovietspy2@yandex.com>
2019-01-31 16:48:13 +0100

    implemented validations properly

--- src/main/java/hu/wortex/report/helpers/ValidationUtility.java
+++ src/main/java/hu/wortex/report/helpers/ValidationUtility.java
@@ -1,10 +1,12 @@
 package hu.wortex.report.helpers;
 
-import hu.wortex.report.entities.Listing;
 import hu.wortex.report.entities.ListingDTO;
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class ValidationUtility {
 
@@ -14,32 +16,57 @@ public class ValidationUtility {
     private List<Integer> marketplaceIds;
     private List<Integer> listingStatusIds;
 
+    private Map<Integer,String> marketplaces;
+
+    private List<List<String>> csvLines;
+
+    /**
+     * validates a JSON parsed DTO
+     * collects lines with errors
+     * @param listing a listing dto that is about to be saved
+     * @return valid or not
+     */
     public boolean isValidListing(ListingDTO listing) {
 
         if (!listing.getId().matches(REGEX_PATTERN)) {
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.ID);
             return false;
         } else if (listing.getTitle() == null) {
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.TITLE);
             return false;
         } else if (listing.getDescription() == null) {
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.DESCRIPTION);
             return false;
         } else if (listing.getLocationId() == null || !locationIds.contains(listing.getLocationId())) { // meg kell nezni a DB-ben
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.LOCATION_ID);
             return false;
-        } else if (listing.getListingPrice() == null || listing.getListingPrice() < 0) {// 2 tizedesjegy
+        } else if (listing.getListingPrice() == null || listing.getListingPrice() < 0 || !precisionCheck(listing.getListingPrice())) {// 2 tizedesjegy
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.LISTING_PRICE);
             return false;
-        } else if (listing.getCurrency() == null || listing.getCurrency().length() != 3) {// vagy nem pontosan 3 karakter
+        } else if (listing.getCurrency() == null || listing.getCurrency().length() != 3) {// vagy nem pontosan 3 karakter //
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.CURRENCY);
             return false;
         } else if (listing.getQuantity() == null || listing.getQuantity() < 0) {
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.QUANTITY);
             return false;
         } else if (listing.getListingStatusId() == null || !listingStatusIds.contains(listing.getListingStatusId())) { // meg kell nezni db
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.LISTING_STATUS);
             return false;
         } else if (listing.getMarketPlaceId() == null || !marketplaceIds.contains(listing.getMarketPlaceId())) { // meg kell nezni db
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.MARKETPLACE);
             return false;
         } else if (listing.getOwnerEmailAddress() == null || !isValidEmail(listing.getOwnerEmailAddress())) {
+            appendCsvLines(listing.getId(), listing.getMarketPlaceId(), Field.LISTING_OWNER_EMAIL);
             return false;
         }
         return true;
     }
 
+    /**
+     *  trims and validates an email address
+     * @param email
+     * @return is valid or not
+     */
     private boolean isValidEmail(String email) {
         email = email.trim();
         EmailValidator eValidator = EmailValidator.getInstance();
@@ -48,11 +75,49 @@ public class ValidationUtility {
 
     public ValidationUtility(List<String> locationIds,
                              List<Integer> marketplaceIds,
-                             List<Integer> listingStatusIds) {
+                             List<Integer> listingStatusIds,
+                             Map<Integer,String> marketplaces) {
 
         this.locationIds = locationIds;
         this.listingStatusIds = listingStatusIds;
         this.marketplaceIds = marketplaceIds;
+        this.marketplaces = marketplaces;
+        csvLines = new ArrayList<>();
+    }
 
+    public List<List<String>> getCsvLines() {
+        return csvLines;
+    }
+
+    /**
+     * We want to decide wheter its precision is less then 2 or not
+     * @param d
+     * @return is valid or not
+     */
+    private boolean precisionCheck(Double d) {
+        if (d!=null) {
+            String text = Double.toString(Math.abs(d));
+            int integerPlaces = text.indexOf('.');
+            int decimalPlaces = text.length() - integerPlaces - 1;
+            return decimalPlaces<=2;
+        }
+        return false;
+    }
+
+    /**
+     *  adds a List<String> that represents a row in the CSV
+     * @param id Listingid
+     * @param marketplaceId
+     * @param field field name with error
+     */
+    private void appendCsvLines(String id, Integer marketplaceId, Field field) {
+        String marketplaceName = marketplaces.get(marketplaceId);
+        csvLines.add(Arrays.asList(id, marketplaceName, field.getFieldName()));
+    }
+
+    //
+    private void appendCsvLinesWithFieldValue(String id, Integer marketplaceId, Field field, String value) {
+        String marketplaceName = marketplaces.get(marketplaceId);
+        csvLines.add(Arrays.asList(id, marketplaceName, field.getFieldName(),value));
     }
 }
